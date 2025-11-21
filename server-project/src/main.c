f31fc713453cb32f0660f8488284c4d256902573
Claudio140406 <claudioandriani472@gmail.com>
2025-11-21 12:53:55 +0100

    modifica

--- server-project/src/main.c
+++ server-project/src/main.c
@@ -1,10 +1,8 @@
 /*
- * main.c
+ * server.c
  *
- * TCP Server - Template for Computer Networks assignment
- *
- * This file contains the boilerplate code for a TCP server
- * portable across Windows, Linux and macOS.
+ * TCP Server - Servizio Meteo
+ * Portabile su Windows, Linux e macOS
  */
 
 #if defined WIN32
@@ -18,61 +16,149 @@
 #include <netinet/in.h>
 #include <netdb.h>
 #define closesocket close
+#define strcasecmp _stricmp
 #endif
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <ctype.h>
 #include "protocol.h"
 
-#define NO_ERROR 0
+// Lista città supportate
+const char *citta[] = {
+    "Bari", "Roma", "Milano", "Napoli", "Torino",
+    "Palermo", "Genova", "Bologna", "Firenze", "Venezia"
+};
+const int num_cities = sizeof(citta) / sizeof(citta[0]);
+
+// Validazione città (case-insensitive)
+int citta_valida(const char *c) {
+    for (int i = 0; i < num_cities; i++) {
+        if (strcasecmp(c, citta[i]) == 0) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+// Funzioni di generazione dati meteo
+float get_temperature(void) {
+    return ((float)rand() / RAND_MAX) * (40.0 - (-10.0)) + (-10.0);
+}
+float get_humidity(void) {
+    return ((float)rand() / RAND_MAX) * (100.0 - 20.0) + 20.0;
+}
+float get_wind(void) {
+    return ((float)rand() / RAND_MAX) * (100.0 - 0.0) + 0.0;
+}
+float get_pressure(void) {
+    return ((float)rand() / RAND_MAX) * (1050.0 - 950.0) + 950.0;
+}
 
 void clearwinsock() {
 #if defined WIN32
-	WSACleanup();
+    WSACleanup();
 #endif
 }
 
-int main(int argc, char *argv[]) {
-
-	// TODO: Implement server logic
-
+int main() {
 #if defined WIN32
-	// Initialize Winsock
-	WSADATA wsa_data;
-	int result = WSAStartup(MAKEWORD(2,2), &wsa_data);
-	if (result != NO_ERROR) {
-		printf("Error at WSAStartup()\n");
-		return 0;
-	}
+    WSADATA wsa_data;
+    int result = WSAStartup(MAKEWORD(2, 2), &wsa_data);
+    if (result != NO_ERROR) {
+        printf("Errore a WSAStartup()\n");
+        exit(EXIT_FAILURE);
+    }
 #endif
 
-	int my_socket;
+    srand(time(NULL));
 
-	// TODO: Create socket
-	// my_socket = socket(...);
+    // Creazione socket
+    int my_socket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (my_socket < 0) {
+        printf("Creazione socket fallita.\n");
+        clearwinsock();
+        exit(EXIT_FAILURE);
+    }
 
-	// TODO: Configure server address
-	// struct sockaddr_in server_addr;
-	// server_addr.sin_family = AF_INET;
-	// server_addr.sin_port = htons(SERVER_PORT);
-	// server_addr.sin_addr.s_addr = INADDR_ANY;
+    // Configurazione indirizzo
+    struct sockaddr_in sad;
+    sad.sin_family = AF_INET;
+    sad.sin_addr.s_addr = inet_addr("127.0.0.1");
+    sad.sin_port = htons(SERVER_PORT);
 
-	// TODO: Bind socket
-	// bind(...);
+    // Bind
+    if (bind(my_socket, (struct sockaddr*)&sad, sizeof(sad)) < 0) {
+        printf("bind() fallita.\n");
+        closesocket(my_socket);
+        clearwinsock();
+        exit(EXIT_FAILURE);
+    }
 
-	// TODO: Set socket to listen
-	// listen(...);
+    // Listen
+    if (listen(my_socket, QUEUE_SIZE) < 0) {
+        printf("listen() fallita.\n");
+        closesocket(my_socket);
+        clearwinsock();
+        exit(EXIT_FAILURE);
+    }
 
-	// TODO: Implement connection acceptance loop
-	// while (1) {
-	//     int client_socket = accept(...);
-	//     // Handle client communication
-	//     closesocket(client_socket);
-	// }
+    printf("Server in ascolto sulla porta %d...\n", SERVER_PORT);
 
-	printf("Server terminated.\n");
+    // Ciclo infinito per gestire i client
+    while (1) {
+        struct sockaddr_in cad;
+        int client_len = sizeof(cad);
+        int client_socket = accept(my_socket, (struct sockaddr*)&cad, &client_len);
+        if (client_socket < 0) {
+            printf("accept() fallita.\n");
+            continue;
+        }
 
-	closesocket(my_socket);
-	clearwinsock();
-	return 0;
-} // main end
+        printf("Connessione accettata da %s\n", inet_ntoa(cad.sin_addr));
+
+        weather_request_t rich;
+        weather_response_t ris;
+
+        // Ricezione richiesta
+        if (recv(client_socket, (char*)&rich, sizeof(rich), 0) <= 0) {
+            perror("Errore recv");
+            closesocket(client_socket);
+            continue;
+        }
+
+        printf("Richiesta '%c %s' dal client ip %s\n", rich.type, rich.city, inet_ntoa(cad.sin_addr));
+
+        // Validazione richiesta
+        if (rich.type != 't' && rich.type != 'h' && rich.type != 'w' && rich.type != 'p') {
+            ris.status = STATUS_INVALID_REQUEST;
+            ris.type = '\0';
+            ris.value = 0.0;
+        } else if (!citta_valida(rich.city)) {
+            ris.status = STATUS_CITY_NOT_AVAILABLE;
+            ris.type = '\0';
+            ris.value = 0.0;
+        } else {
+            ris.status = STATUS_SUCCESS;
+            ris.type = rich.type;
+            switch (rich.type) {
+                case 't': ris.value = get_temperature(); break;
+                case 'h': ris.value = get_humidity(); break;
+                case 'w': ris.value = get_wind(); break;
+                case 'p': ris.value = get_pressure(); break;
+            }
+        }
+
+        // Invio risposta
+        send(client_socket, (char*)&ris, sizeof(ris), 0);
+
+        // Chiusura connessione client
+        closesocket(client_socket);
+    }
+
+    // Chiusura socket server
+    closesocket(my_socket);
+    clearwinsock();
+    return 0;
+}
--- server-project/src/protocol.h
+++ server-project/src/protocol.h
@@ -1,24 +1,46 @@
 /*
  * protocol.h
  *
- * Server header file
- * Definitions, constants and function prototypes for the server
+ * Header file condiviso tra client e server
+ * Definizioni, costanti, strutture e prototipi
  */
 
 #ifndef PROTOCOL_H_
 #define PROTOCOL_H_
 
-// Shared application parameters
-#define SERVER_PORT 27015  // Server port (change if needed)
-#define BUFFER_SIZE 512    // Buffer size for messages
-#define QUEUE_SIZE 5       // Size of pending connections queue
+#include <stdbool.h>
 
-// Function prototypes
-// Add here the signatures of the functions implemented by students
+// Parametri condivisi
+#define SERVER_PORT 56700   // Porta di default del server
+#define BUFFER_SIZE 512     // Dimensione buffer per messaggi
+#define QUEUE_SIZE 5        // Dimensione della coda di connessioni pendenti
+#define NO_ERROR 0
 
-/*
- * Example function to implement:
- * int handle_client(int client_socket);
- */
+// Codici di stato della risposta
+#define STATUS_SUCCESS 0
+#define STATUS_CITY_NOT_AVAILABLE 1
+#define STATUS_INVALID_REQUEST 2
+
+// Struttura richiesta (Client → Server)
+typedef struct {
+    char type;        // Weather data type: 't', 'h', 'w', 'p'
+    char city[64];    // Nome città (null-terminated string)
+} weather_request_t;
+
+// Struttura risposta (Server → Client)
+typedef struct {
+    unsigned int status;  // Codice di stato
+    char type;            // Eco del tipo richiesto
+    float value;          // Valore meteo
+} weather_response_t;
+
+// Funzioni di generazione dati meteo (server)
+float get_temperature(void);    // Range: -10.0 to 40.0 °C
+float get_humidity(void);       // Range: 20.0 to 100.0 %
+float get_wind(void);           // Range: 0.0 to 100.0 km/h
+float get_pressure(void);       // Range: 950.0 to 1050.0 hPa
+
+// Funzione di validazione città (server)
+int citta_valida(const char *c);
 
 #endif /* PROTOCOL_H_ */
